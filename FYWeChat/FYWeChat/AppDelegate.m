5ebb816a837150b9eb2fb5889ec0d382dae3653d
fengyanyan <fengyanyanme@live.com>
2015-07-19 13:15:21 +0800

    环境配置，登陆步骤的实现

--- FYWeChat/FYWeChat/AppDelegate.m
+++ FYWeChat/FYWeChat/AppDelegate.m
@@ -7,9 +7,26 @@
 //
 
 #import "AppDelegate.h"
-
+#import "XMPPFramework.h"
 @interface AppDelegate ()
-
+{
+    XMPPStream *_xmppStream;
+}
+/*
+ 登陆的实现：
+ 1.初始化XMPPStream类
+ 2.连接到服务器  传一个JID到服务器
+ 3.连接到服务器成功以后，发送密码 进行授权
+ 4.授权成功之后，发送“在线”消息
+ */
+// 1.初始化XMPPStream类
+-(void)SetUpXMPPStream;
+//2.连接到服务器  传一个JID到服务器
+-(void)ConnectToHost;
+//3.连接到服务器成功以后，发送密码 进行授权
+-(void)SendPasswordToHost;
+//4.授权成功之后，发送“在线”消息
+-(void)SenOnlineToHost;
 @end
 
 @implementation AppDelegate
@@ -41,5 +58,93 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
-
+#pragma mark --Private
+//初始化XMPPStream
+-(void)SetUpXMPPStream
+{
+    _xmppStream=[[XMPPStream alloc]init];
+    //    设置代理
+    [_xmppStream addDelegate:self delegateQueue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)];
+}
+//连接到服务器
+-(void)ConnectToHost
+{
+    NSLog(@"开始连接到服务器");
+    if (_xmppStream ==nil)
+    {
+        [self SetUpXMPPStream];
+    }
+    //    设置JID
+    //    resource 标识用户登录的客户端
+    XMPPJID *myJID=[XMPPJID jidWithUser:@"lisi" domain:@"fengyanyan.local" resource:@"iphone"];
+    _xmppStream.myJID=myJID;
+    
+    //    设置服务器的域名
+    _xmppStream.hostName=@"fengyanyan.local";  //不仅可以是域名还可以是IP地址
+    
+    //    设置端口(如果服务器的端口是5222可以省略，5222为默认端口)
+    _xmppStream.hostPort=5222;
+    
+    NSError *error=nil;
+    if (![_xmppStream connectWithTimeout:XMPPStreamTimeoutNone error:&error])
+    {
+        NSLog(@"%@",error);
+    }
+}
+//  发送密码进行授权
+-(void)SendPasswordToHost
+{
+    NSLog(@"发送密码进行授权");
+    NSError *error=nil;
+    [_xmppStream authenticateWithPassword:@"123456" error:&error];
+    if (error)
+    {
+        NSLog(@"%@",error);
+    }
+}
+//授权成功之后，发送“在线”消息
+-(void)SenOnlineToHost
+{
+    NSLog(@"发送在线消息");
+    XMPPPresence *presence=[XMPPPresence presence];
+    [_xmppStream sendElement:presence];
+}
+#pragma mark --XMPPStreamDelegate
+#pragma mark --与主机连接成功
+-(void)xmppStreamDidConnect:(XMPPStream *)sender
+{
+    NSLog(@"与主机连接成功");
+    //    3.发送密码进行授权
+    [self SendPasswordToHost];
+}
+#pragma mark --与主机断开连接
+-(void)xmppStreamDidDisconnect:(XMPPStream *)sender withError:(NSError *)error
+{
+    if (error)
+    {
+        NSLog(@"与主机连接失败 %@",error);
+    }
+}
+#pragma mark --密码发送  授权成功
+-(void)xmppStreamDidAuthenticate:(XMPPStream *)sender
+{
+    NSLog(@"授权成功");
+    //    授权成功之后 发送“在线”消息
+    [self SenOnlineToHost];
+}
+#pragma mark --密码发送  授权失败
+-(void)xmppStream:(XMPPStream *)sender didNotAuthenticate:(DDXMLElement *)error
+{
+    NSLog(@"授权失败");
+}
+#pragma mark -- 公共方法
+-(void)logout
+{
+    //    1.发送“离线”消息
+    XMPPPresence *offline=[XMPPPresence presenceWithType:@"unavailable"];
+    NSLog(@"%@",offline);
+    [_xmppStream sendElement:offline];
+    //    2.与服务器断开连接
+    [_xmppStream disconnect];
+}
 @end
